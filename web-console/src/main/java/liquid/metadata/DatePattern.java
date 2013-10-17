8692067d9a85e8bc81e88c4418218501246fda16
redbrick9 <redbrick9@gmail.com>
2013-10-18 00:33:52 +0800

    Adding date format validator.

---
+++ web-console/src/main/java/liquid/metadata/DatePattern.java
@@ -0,0 +1,20 @@
+package liquid.metadata;
+
+/**
+ * TODO: Comments.
+ * User: tao
+ * Date: 10/17/13
+ * Time: 11:51 PM
+ */
+public enum DatePattern {
+    UNTIL_MINUTE("yyyy-MM-dd HH:mm"),
+    UNTIL_DAY("yyyy-MM-dd");
+
+    private final String pattern;
+
+    private DatePattern(String pattern) {this.pattern = pattern;}
+
+    public String getPattern() {
+        return pattern;
+    }
+}
