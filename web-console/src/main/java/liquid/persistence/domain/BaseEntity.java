9fee051fcd28ee9ca597498db16d6b1d517ae306
redbrick9 <redbrick9@gmail.com>
2013-10-21 23:45:43 +0800

    Fixed Issue #29. Adding create user, update user, create time, update time.

--- web-console/src/main/java/liquid/persistence/domain/BaseEntity.java
+++ web-console/src/main/java/liquid/persistence/domain/BaseEntity.java
@@ -1,6 +1,7 @@
 package liquid.persistence.domain;
 
 import javax.persistence.*;
+import java.util.Date;
 
 /**
  * TODO: Comments.
@@ -15,6 +16,18 @@ public class BaseEntity {
     @Column(name = "ID")
     protected Long id;
 
+    @Column(name = "CREATE_USER")
+    private String createUser;
+
+    @Column(name = "CREATE_TIME")
+    private Date createTime;
+
+    @Column(name = "UPDATE_USER")
+    private String updateUser;
+
+    @Column(name = "UPDATE_TIME")
+    private Date updateTime;
+
     public Long getId() {
         return id;
     }
@@ -23,14 +36,51 @@ public class BaseEntity {
         this.id = id;
     }
 
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
     public boolean isNew() {
         return (this.id == null);
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("BaseEntity{");
+        final StringBuilder sb = new StringBuilder(super.toString());
+        sb.append("BaseEntity{");
         sb.append("id=").append(id);
+        sb.append(", createUser='").append(createUser).append('\'');
+        sb.append(", createTime=").append(createTime);
+        sb.append(", updateUser='").append(updateUser).append('\'');
+        sb.append(", updateTime=").append(updateTime);
         sb.append('}');
         return sb.toString();
     }
