8528ab2331e21457ed8a058154cea54bc9d56f79
redbrick9 <redbrick9@gmail.com>
2013-12-29 20:27:10 +0800

    Adding FormValidationResult.

---
+++ web-console/src/main/java/liquid/validation/FormValidationResult.java
@@ -0,0 +1,39 @@
+package liquid.validation;
+
+/**
+ * Created by redbrick9 on 12/28/13.
+ */
+public class FormValidationResult {
+    private final boolean isSuccessful;
+    private final String messageKey;
+
+    private FormValidationResult(boolean isSuccessful, String messageKey) {
+        this.isSuccessful = isSuccessful;
+        this.messageKey = messageKey;
+    }
+
+    public static FormValidationResult newSuccess() {
+        return new FormValidationResult(true, "");
+    }
+
+    public static FormValidationResult newFailure(String messageKey) {
+        return new FormValidationResult(true, messageKey);
+    }
+
+    public boolean isSuccessful() {
+        return isSuccessful;
+    }
+
+    public String getMessageKey() {
+        return messageKey;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("FormValidationResult{");
+        sb.append("isSuccessful=").append(isSuccessful);
+        sb.append(", messageKey='").append(messageKey).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
