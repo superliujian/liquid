f3d14e2760db269df0760d3e6e6acc5039a427a1
redbrick9 <redbrick9@gmail.com>
2014-05-01 13:46:18 +0800

    Fixing issue #137. Close charge page on order detail.

--- web-console/src/main/java/liquid/api/controller/ApiCustomerController.java
+++ web-console/src/main/java/liquid/api/controller/ApiCustomerController.java
@@ -2,9 +2,15 @@ package liquid.api.controller;
 
 import liquid.persistence.domain.Customer;
 import liquid.service.CustomerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * TODO: Comments.
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/api/customer")
 public class ApiCustomerController {
+    private static final Logger logger = LoggerFactory.getLogger(ApiCustomerController.class);
 
     @Autowired
     private CustomerService customerService;
@@ -28,6 +35,7 @@ public class ApiCustomerController {
     @RequestMapping(method = RequestMethod.GET, params = "name")
     @ResponseBody
     public Iterable<Customer> listByName(@RequestParam String name) {
+        logger.debug("name: {}", name);
         return customerService.findByName(name);
     }
 }
