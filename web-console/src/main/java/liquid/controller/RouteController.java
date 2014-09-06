f90853494d648e4dc8d6f41e0a1874448a1b22eb
redbrick9 <redbrick9@gmail.com>
2014-09-06 12:58:11 +0800

    Adding route detail for order.

--- web-console/src/main/java/liquid/controller/RouteController.java
+++ web-console/src/main/java/liquid/controller/RouteController.java
@@ -1,27 +1,35 @@
 package liquid.controller;
 
+import liquid.order.persistence.domain.OrderEntity;
+import liquid.order.service.OrderService;
+import liquid.shipping.persistence.domain.RouteEntity;
+import liquid.shipping.service.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by redbrick9 on 8/28/14.
  */
 @Controller
-@RequestMapping("/order/{orderId}/route")
+@RequestMapping("/route")
 public class RouteController {
-    public String list(@PathVariable Long orderId) {
-        return "list";
-    }
+    @Autowired
+    private OrderService orderService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String newRoute(@PathVariable Long orderId) {
-        return "redirect:/route";
-    }
+    @Autowired
+    private RouteService routeService;
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.POST, params = "delete")
-    public String deleteRoute(@PathVariable Long id) {
-        return "redirect:/route";
+    @RequestMapping(method = RequestMethod.GET)
+    public String findByOrder(@RequestParam(value = "o") Long orderId, Model model) {
+        OrderEntity order = orderService.find(orderId);
+        Iterable<RouteEntity> routes = routeService.findByOrderId(orderId);
+        model.addAttribute("tab", "route");
+        model.addAttribute("order", order);
+        model.addAttribute("routes", routes);
+        return "route/detail";
     }
 }
