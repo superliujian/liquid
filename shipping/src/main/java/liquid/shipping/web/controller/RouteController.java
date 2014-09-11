7523c1107cec46c8fe7d89a9eb7100b228298ace
redbrick9 <redbrick9@gmail.com>
2014-09-11 22:58:41 +0800

    for route edit of order detail.

--- shipping/src/main/java/liquid/shipping/web/controller/RouteController.java
+++ shipping/src/main/java/liquid/shipping/web/controller/RouteController.java
@@ -4,12 +4,17 @@ import liquid.order.persistence.domain.OrderEntity;
 import liquid.order.service.OrderService;
 import liquid.shipping.persistence.domain.RouteEntity;
 import liquid.shipping.service.RouteService;
+import liquid.shipping.web.domain.Routes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by redbrick9 on 9/11/14.
@@ -33,13 +38,26 @@ public class RouteController {
         return "route/detail";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET, params = "action=edit")
     public String initEditForm(@RequestParam(value = "o") Long orderId, Model model) {
         OrderEntity order = orderService.find(orderId);
-        Iterable<RouteEntity> routes = routeService.findByOrderId(orderId);
+        Iterable<RouteEntity> routeEntities = routeService.findByOrderId(orderId);
+
+        Routes routes = new Routes();
+
         model.addAttribute("tab", "route");
         model.addAttribute("order", order);
         model.addAttribute("routes", routes);
-        return "route/detail";
+        return "route/edit";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String edit(@RequestParam(value = "o") Long orderId, @ModelAttribute Routes routes) {
+
+
+        Collection<RouteEntity> routeEntities = new ArrayList<>();
+        routeService.save(routeEntities);
+
+        return "redirect:/route?o=" + orderId;
     }
 }
--- shipping/src/main/java/liquid/shipping/web/domain/Routes.java
+++ shipping/src/main/java/liquid/shipping/web/domain/Routes.java
@@ -1,5 +1,10 @@
 package liquid.shipping.web.domain;
 
+import liquid.shipping.persistence.domain.RouteEntity;
+
+import java.util.Collection;
+import java.util.Collections;
+
 /**
  * Created by redbrick9 on 9/11/14.
  */
@@ -7,4 +12,12 @@ public class Routes {
     private Long orderId;
 
     private RailTransport[] railTransport;
+
+    public Collection<RouteEntity> toEntities() {
+        return Collections.emptyList();
+    }
+
+    public static void valueOf(Collection<RouteEntity> entities) {
+        
+    }
 }
